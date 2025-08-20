ea40ed54da4a6eca7a8349a045f954b8ce73dbb6
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-08-20 17:33:25 +0900

    Fix Bazel

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,10 +33,12 @@ write_to_bazelrc "test --python_path=\"${PYTHON_BIN}\""
 write_to_bazelrc "common --check_direct_dependencies=error"
 
 # Cross-compiling with bazel-toolchain
-write_to_bazelrc "build --crosstool_top=//bazel_toolchain:toolchain"
-write_to_bazelrc "build --cpu=\"${TARGET_CPU}\""
 write_to_bazelrc "build --platforms=//bazel_toolchain:target_platform"
 write_to_bazelrc "build --host_platform=//bazel_toolchain:build_platform"
+write_to_bazelrc "build --extra_toolchains=//bazel_toolchain:cc_cf_toolchain"
+write_to_bazelrc "build --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain"
+write_to_bazelrc "build --crosstool_top=//bazel_toolchain:toolchain"
+write_to_bazelrc "build --cpu=\"${TARGET_CPU}\""
 write_to_bazelrc "build --logging=6"
 write_to_bazelrc "build --verbose_failures"
 
